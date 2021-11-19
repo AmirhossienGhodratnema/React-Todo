bf246dfdc61c0cfb3382043a1e133d08318f0acb
AmirhosseinGhodratnema <amirhosseinghodratnema@gmail.com>
2021-11-19 14:18:26 +0330

    Configuration is ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import react, {PureComponent} from 'react'
+import './css/reset.css';
+import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div>
+
+                <div>
+                    <div className="header bg-dark d-flex justify-content-center">
+                        <h5 className="text-white p-2 w-50">TodoApp</h5>
+                    </div>
+                </div>
+
+                <div className="Main-input bg-light d-flex justify-content-center p-5">
+                    <div>
+                        <h2>Welcome!</h2>
+                        <p>To get started, add some items to your list:</p>
+                        <div className="inp-btn d-flex justify-content-center align-content-between">
+                            <input className="form-control mr-3 " type="text"/>
+                            <button className="btn btn-primary" type="submit">add</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="d-flex flex-column justify-content-center align-items-center mt-4">
+                    <nav className="w-25">
+                        <div className="nav nav-tabs">
+                            <a className="nav-item nav-link active">Home <span className="badge badge-danger">4</span></a>
+                            <a className="nav-item nav-link">Profile <span className="badge badge-success">4</span></a>
+                        </div>
+                    </nav>
+                    <div className="tab-content w-25 mt-3">
+                        <div className=" w-100 tab-pane fade show active">
+                            <class className="mb-2 d-flex justify-content-between border rounded-3 p-2 align-items-center ">
+                                <h5 className="m-0">Hello Roocket</h5>
+                                <div>
+                                    <button className="btn btn-info btn-sm">Edit</button>
+                                    <button className="btn btn-danger btn-sm">Delet</button>
+                                </div>
+                            </class>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
